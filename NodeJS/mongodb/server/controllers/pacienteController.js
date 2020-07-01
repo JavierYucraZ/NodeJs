d27ff38ec918f46f4979189623d4afb04790d115
javieryucraz <xavwlr20@gmail.com>
2020-06-30 21:07:19 -0400

    c12

--- NodeJS/mongodb/server/controllers/pacienteController.js
+++ NodeJS/mongodb/server/controllers/pacienteController.js
@@ -3,7 +3,5 @@ const Paciente = require('../models/paciente')
 exports.nuevoCliente = async (req, res) => {
     const paciente = new Paciente(req.body)
     await paciente.save()
-    res.json({
-        mensaje : 'El cliente se agrego correctamente'
-    })
+    res.redirect('/')
 }
--- NodeJS/mongodb/server/routes/index.js
+++ NodeJS/mongodb/server/routes/index.js
@@ -5,6 +5,7 @@ const PacienteModel = require('../models/paciente')
 
 module.exports = function(){
 
+    //Listar todos los pacientes en el index
     router.get('/', async (req, res) => {
         const listaPacientes = await PacienteModel.find()
         res.render('index', {
@@ -12,9 +13,37 @@ module.exports = function(){
         })
     })
 
+    //Agregar un nuevo registro
     router.post('/agregar', 
         pacienteController.nuevoCliente
     )
+
+    //Eliminar un registro
+    router.get('/eliminar/:id', async (req, res) => {
+        const { id } = req.params
+        await PacienteModel.deleteOne({ _id : id })
+        res.redirect('/')
+    })
+
+    //Realizamos la busqueda por id
+    //Renderizamos la pagina de editar
+    //Enviamos la informacion del paciente encontrado
+    router.get('/editar/:id', async (req, res) => {
+        const { id } = req.params
+        const paciente = await PacienteModel.findById(id)
+        res.render('editar', {
+            paciente
+        })
+    })
+
+    //Obtiene el id por parametro
+    //Compara con los id de la base de datos
+    //Reemplaza el objeto encontrado con la nueva informacion
+    router.post('/editar/:id', async(req, res) => {
+        const { id } = req.params
+        await PacienteModel.updateOne({ _id : id }, req.body)
+        res.redirect('/') 
+    })
 
     return router
 }
